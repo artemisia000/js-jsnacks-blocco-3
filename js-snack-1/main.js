86195f5efdce68d0bfde19051ea8fd408f73320b
artemisia000 <misiapierobon@gmail.com>
2021-11-08 17:52:19 +0100

    randWord

--- js-snack-1/main.js
+++ js-snack-1/main.js
@@ -24,16 +24,19 @@ const player = [
         name: 'Kobe',
         lastName: 'Bryant',
         age: '43',
-        media: '30',
+        media: '36',
         percentage: '60%',
     },
 ];
 console.table(player);
 
 
+for(let i = 0; i < 3; i++){
 
-//R
+    const randCode = getRandom(0, 10);
+    console.log('randCode',randCode);
 
+}
 
 
 
@@ -44,19 +47,34 @@ for(let i = 0; i < 1; i++ ){
 
     //invocazione
     const randMedia = getRandom(0,50);
-    console.log(randMedia);
-}
+    console.log('randMedia',randMedia);
+
 
 //RAND percentuale tiri a 3 punti
     
     //invocazione
 const randPunti = getRandom(0, 1003);
-console.log(randPunti);
+console.log('randPunti',randPunti);
+}
 
 
-//definizione funzione
+//definizione funzione numeri random
 
 function getRandom(min,max){
     const rand = Math.floor(Math.random() * (max - min + 1))+ min;
     return rand;
 }
+
+//definizione funzione lettere random
+
+function randWord(length) {
+    let result = '';
+    let word = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let wordLength = word.length;
+    for ( let i = 0; i < length; i++ ) {
+       result += word.charAt(Math.floor(Math.random() * wordLength));
+    }
+    return result;
+ }
+ 
+ console.log(randWord(3));
