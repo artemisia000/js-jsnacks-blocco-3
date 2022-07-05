3840168b68702eddabc95885490b958fb35d2205
artemisia000 <misiapierobon@gmail.com>
2022-07-05 15:30:50 +0200

    ok

--- js-snack-1/main.js
+++ js-snack-1/main.js
@@ -10,164 +10,142 @@
 //- Percentuale di successo per tiri da 3 punti2. Generare tramite delle funzioni le statistiche di gioco, secondo queste regole:
 //- il codice giocatore deve essere formato da 3 lettere maiuscole casuali e 3 cifre casuali
 //- la media punti fatti per partita deve essere compresa tra 0 e 50
-//- la percentuale di successo per tiri da 3 punti deve essere compresa tra 0 e 1003. Stampare Nome, cognome, età e codice giocatore.
+//- la percentuale di successo per tiri da 3 punti deve essere compresa tra 0 e 100. Stampare Nome, cognome, età e codice giocatore.
 //4. Creare un array di 10 giocatori di basket, con le regole sopra indicate
 //5. Creare un nuovo array con i giocatori che hanno una media di punti superiore a 35 e la percentuale di successo per i tiri da 3 punti superiore all’80%.
 
 
 
-//creare object giocatore
+//.1
+//.4
 
 const player = [
     {
-        code: 'AIS024',
+        code: getCode(),
         name: 'Kobe',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 43,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Albert',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 23,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
+        code: getCode(),
         name: 'Kobe',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 43,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Mister',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 43,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Andre',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 28,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Malù',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 33,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Lucas',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 40,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Paul',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 25,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Mapo',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
+        age: 18,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
     },
     {
-        code: 'AIS024',
-        name: 'Kobe',
+        code: getCode(),
+        name: 'Koli',
         lastName: 'Bryant',
-        age: '43',
-        media: '23',
-        percentage: '60%',
-    },
-
+        age: 33,
+        media: getRandom(0,50),
+        percentage: getRandom(0,100),
+    }
 ];
-
-
-
-
 
 console.table(player);
 
 
-const cardPlayer = player.map( (play) => {
-    return play.code;
-});
-
-console.log(cardPlayer);
+//.3
+const { name , lastName , age , code} = player;
+// console.log(`Il giocatore ${name} ${lastName} ha ${age} anni ed il suo codice è ${code}`);
 
 
 
+//.5
+const totPlayers = player.filter( (player) => player.media > 35 && player.percentage > 80 );
+console.table(totPlayers);
+console.log(totPlayers.length);
 
-for(let i = 0; i < 3; i++){
 
-    
 
-    const randCode = getRandom(0, 10);
-    console.log('randCode',randCode);
+/**
+ * Genera numeri interi
+ */
 
+function getRandom(min , max) {
+    return Math.floor(Math.random() * (max - min + 1 ) ) + min;
+};
+
+
+/**
+ * Genera codice Player
+ */
+function getCode() {
+    const chars = 'ABCDEFGHILMNOPQRSTUVZ';
+   
+
+    let getChars = '';
+    let getNum = '';
+
+    for(let i = 0; i < 3; i++) {
+        //chars
+        getChars += chars.charAt( getRandom(0, chars.length - 1) );
+
+        //numbers
+        getNum += getRandom(0, 9);
+    }  
+    return getChars + getNum;
 }
 
 
 
-
-
-//RAND media punti per partita
-
-for(let i = 0; i < 1; i++ ){
-    //genera numero random
-
-    //invocazione
-    const randMedia = getRandom(0,50);
-    console.log('randMedia',randMedia);
-
-
-//RAND percentuale tiri a 3 punti
-    
-    //invocazione
-const randPunti = getRandom(0, 1003);
-console.log('randPunti',randPunti);
-}
-
-
-//definizione funzione numeri random
-
-function getRandom(min,max){
-    const rand = Math.floor(Math.random() * (max - min + 1))+ min;
-    return rand;
-}
-
-//definizione funzione lettere random
-
-function randWord(length) {
-    let result = '';
-    let word = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let wordLength = word.length;
-    for ( let i = 0; i < length; i++ ) {
-       result += word.charAt(Math.floor(Math.random() * wordLength));
-    }
-    return result;
- }
- 
- console.log(randWord(3));
-
- 
